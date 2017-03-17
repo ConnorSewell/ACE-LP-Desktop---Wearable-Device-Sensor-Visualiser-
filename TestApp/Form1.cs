23777e576194c0ab650a5aee76dd7f67d6a736a7
ConnorSewell <ctrsewell@dundee.ac.uk>
2017-03-17 03:50:06 +0000

    Initial Commit

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace TestApp
 {
+    using OxyPlot;
+    using OxyPlot.Series;
+
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+           
         }
     }
 }
