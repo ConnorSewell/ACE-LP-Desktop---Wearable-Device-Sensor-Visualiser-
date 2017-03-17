fc7ffe2af95b6df3a7f01abe33449804ccb3f9c2
ConnorSewell <ctrsewell@dundee.ac.uk>
2017-03-17 14:46:52 +0000

    Parsing dummy text file and creating graph.

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -12,6 +12,8 @@ namespace TestApp
 {
     using OxyPlot;
     using OxyPlot.Series;
+    using System.IO;
+    
 
     public partial class Form1 : Form
     {
@@ -22,9 +24,86 @@ namespace TestApp
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           
-           
+
+        }
+
+        private void loadDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //Using http://stackoverflow.com/questions/11624298/how-to-use-openfiledialog-to-select-a-folder
+            //^ For opening folder and parsing file names. Accessed: 16/03/2017 @ 20:10
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+            DialogResult result = openFolderDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] sensorFiles = Directory.GetFiles(openFolderDialog.SelectedPath);
+                for (int i = 0; i < sensorFiles.Length; i++)
+                {
+                    string sensorFile = sensorFiles[i].Substring(sensorFiles[i].LastIndexOf("\\") + 1);
+
+                    switch (sensorFile)
+                    {
+                        case "AccelerometerData.txt":
+                            parseAndCreateGraph(sensorFiles[i]);
+                            break;
+                        case "GyroscopeData.txt":
+                            break;
+                        default:
+                            MessageBox.Show(sensorFile + " is not a valid Sensor Data File");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void parseAndCreateGraph(string filePath)
+        {
+            //https://www.youtube.com/watch?v=VC-nlI_stx4
+            //^ Temp ref
+            OxyPlot.WindowsForms.PlotView pv = new OxyPlot.WindowsForms.PlotView();
+            pv.Dock = DockStyle.Left;
+            this.Controls.Add(pv);
+
+            OxyPlot.Axes.LinearAxis xAxis = new OxyPlot.Axes.LinearAxis();
+            xAxis.Position = OxyPlot.Axes.AxisPosition.Bottom;
+            xAxis.Maximum = 200;
+            xAxis.Minimum = 0;
+
+            OxyPlot.Axes.LinearAxis yAxis = new OxyPlot.Axes.LinearAxis();
+            yAxis.Position = OxyPlot.Axes.AxisPosition.Left;
+
+            FunctionSeries fs = new FunctionSeries();
+            PlotModel pm = new PlotModel();
+
+            String line;
+            StreamReader file = new StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] parsedLine = line.Split(',');
+                fs.Points.Add(new DataPoint(Convert.ToDouble(parsedLine[0]), Convert.ToDouble(parsedLine[1])));
+            }
+          
+            pm.Series.Add(fs);
+            pm.Axes.Add(xAxis);
+            pm.Axes.Add(yAxis);
+            pv.Model = pm;
+
+            file.Close();
+        }
+
+        private void loadVideo(string filePath)
+        {
+
         }
     }
+
+  
 }
+
+
+           
+
